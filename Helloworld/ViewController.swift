d5f408cc76cf9bf1d49f86cdc640246111719b90
Andrew <xvfv@gmail.com>
2022-05-01 19:45:22 +0300

    add hideshow text

--- Helloworld/ViewController.swift
+++ Helloworld/ViewController.swift
@@ -8,13 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var Helloworldlabel: UILabel!
+    @IBOutlet var toggleButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        Helloworldlabel.isHidden = true
+        toggleButton.layer.cornerRadius = 10
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func makeButtonAction() {
+        if Helloworldlabel.isHidden {
+            Helloworldlabel.isHidden = false
+            toggleButton.setTitle("Hide Text", for: .normal)
+            
+        } else {
+            Helloworldlabel.isHidden = true
+            toggleButton.setTitle("Show text", for: .normal)
+        }
+        
+    }
 
 }
-//fdvdfvdfvdfvdfdvfdfvd
-
