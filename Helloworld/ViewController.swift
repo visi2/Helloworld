981d1591e104100765433b7bdc45e1286d1d09db
Andrei <kavsha007@mail.ru>
2022-08-25 16:06:37 +0300

    Auto stash before gghchecking out "HEAD"

--- Helloworld/ViewController.swift
+++ Helloworld/ViewController.swift
@@ -11,14 +11,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-<<<<<<< 7fc526816cfb3cc7ad46ae055dc312bbaf39bb43
-        // Do any additional setup after loading the view.
-=======
+    
         
         Helloworldlabel.isHidden = true
         toggleButton.layer.cornerRadius = 10
         // Do any additional setup after erferferfefloading the view.
->>>>>>> nk nk
+
     }
 
 
